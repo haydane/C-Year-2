a221e44faa7e4a318210f679f64864aa01b2c3f4
unknown <haydane9@gmail.com>
2017-05-09 19:11:32 +0700

    file

--- file4.cpp
+++ file4.cpp
@@ -1,33 +1,92 @@
 #include <iostream.h>
 #include <fstream.h>
-#include <fstream.h>
-#include <iostream.h>
-class per
+#include <string.h>
+class Employee
 {
-public:
+private:
+	int id,age;
 	char name[20];
-	int age;
+	float rate,hours;
 public:
-	void input()
+	Employee()
 	{
-		cout << "Name: ";
-		cin.seekg(0);
-		cin.get(name,20);
-		cout << "Age: ";
-		cin >> age;
+		id=0;
+		strcpy(name,"Dane");
+		age=20;
+		rate=0.0;
+		hours=0.0;
 	}
-	void output()
-	{
-		cout << "Name: " << name << endl;
-		cout << "Age: " << age << endl;
-	}
+	Employee(int i,char *n,int a,float r,float h);
+	friend ostream &operator << (ostream& out, Employee &emp);
+	friend istream &operator >> (istream& in, Employee &emp);
+	float income();
 };
+
+Employee::Employee(int i,char *n,int a,float r,float h)
+{
+	id = i;
+	strcpy(name,n);
+	age = a;
+	rate = r;
+	hours = h;
+}
+ostream &operator << (ostream& out, Employee &emp)
+{
+	out  << "ID: " << emp.id << endl;
+	out << "Name: " << emp.name << endl;
+	out << "Age: " << emp.age << endl;
+	out << "Rate: " << emp.rate << endl;
+	out << "Hours: " << emp.hours << endl;
+	out << "RH: " << emp.income() << endl;
+	return out;
+}
+istream &operator >> (istream& in, Employee &emp)
+{
+	cout << "ID: ";
+	in >> emp.id;
+	cout << "Name: ";
+	in.clear();
+	in.seekg(0,ios::end);
+	in.get(emp.name,20);
+	cout << "Age: ";
+	in >> emp.age;
+	cout << "Rate: ";
+	in >> emp.rate;
+	cout << "Hours: ";
+	in >> emp.hours;
+	return in;
+}
+float Employee::income()
+{
+	return (rate*hours);
+}
 void main()
 {
-	per r;
-	//r.input();
-	fstream file;
-	file.open("afile.txt");
-	file.read((char*)(&r),sizeof(r));
-	r.output();
+	system("cls");
+	Employee e[20];
+	int n,i;
+	cout << "Enter n: ";
+	cin >> n;
+	for(i=0;i<n;i++)
+		cin >> e[i];
+	ofstream out("emp.dat",ios::binary);
+	if(out.fail())
+	{
+		cout << "Error can not open file\n";
+		return;
+	}
+	out.write((char*)&e,n*sizeof(Employee));
+	out.close();
+	ifstream in("emp.dat",ios::binary);
+	if(in.fail())
+	{
+		cout << "Error cannot open file\n";
+		return;
+	}
+	in.read((char*)(&e),n*sizeof(Employee));
+	for(i=0;i<n;i++)
+	{
+		cout << e[i];
+	}
+	in.close();
 }
